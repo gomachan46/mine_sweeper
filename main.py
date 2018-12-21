ca0fa6dd9dba0912791144ec78cb3d73fb4a72d9
gomachan46 <shiro.gomachan46@gmail.com>
2018-12-21 15:21:54 +0900

    セルの開閉

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ import random
 def main():
     side_length = 9
     bomb_count = 10
-    field = [[{'type': 'Safe', 'count': 0} for _ in range(side_length)] for _ in range(side_length)]
+    field = [[{'type': 'Safe', 'count': 0, 'visible': False} for _ in range(side_length)] for _ in range(side_length)]
 
     candidates = range(side_length)
     for _ in range(bomb_count):
         a, b = random.sample(candidates, 2)
-        field[a][b] = {'type': 'Bomb'}
+        field[a][b] = {'type': 'Bomb', 'visible': False}
 
     for i in range(side_length):
         for j in range(side_length):
@@ -16,11 +16,23 @@ def main():
                 continue
 
             surroundings = [(i - 1, j - 1), (i, j - 1), (i + 1, j - 1), (i - 1, j), (i + 1, j), (i - 1, j + 1), (i, j + 1), (i + 1, j + 1)]
-            for a, b in surroundings:
-                if a in range(side_length) and b in range(side_length) and field[a][b]['type'] == 'Safe':
-                    field[a][b]['count'] += 1
+            for r, c in surroundings:
+                if r in range(side_length) and c in range(side_length) and field[r][c]['type'] == 'Safe':
+                    field[r][c]['count'] += 1
+
+    def open_cell(r, c):
+        field[r][c]['visible'] = True
+
+    open_cell(8, 4)
+    open_cell(8, 5)
+    open_cell(7, 4)
 
     for rows in field:
-        print(' '.join([str(c['count']) if c['type'] == 'Safe' else 'B' for c in rows]))
+        def write_cell(c):
+            if not c['visible']:
+                return ' '
+            return str(c['count']) if c['type'] == 'Safe' else 'B'
+
+        print(' '.join(map(write_cell, rows)))
 
 main()
