155df390e9b36b7a9e245ec99fad71cb05a281cd
gomachan46 <shiro.gomachan46@gmail.com>
2019-01-02 12:53:06 +0900

    ショップができた

--- scenes/shop.py
+++ scenes/shop.py
@@ -8,7 +8,7 @@ class Shop(Scene):
     def __init__(self, player, next_level):
         self.__player = player
         self.__next_level = next_level
-        self.cursor = 0
+        # ショップ、そんなに拡張性いらないと思うので5x6マスで固定して考える
         self.__parts_lines = [
             [
                 Power(5),
@@ -53,17 +53,64 @@ class Shop(Scene):
         ]
 
     def draw(self):
+        decorated_lines = []
         for line in self.__parts_lines:
-            Canvas.store([' '.join([f'[{part.name}]' for part in line])])
+            decorated_lines.append(['うりきれ' if part is None else part.name for part in line])
+        decorated_lines[self.__player.y][self.__player.x] = f'[{decorated_lines[self.__player.y][self.__player.x]}]'
+        for line in decorated_lines:
+            Canvas.store([' '.join([part_name for part_name in line])])
+
         Canvas.store([
             'いらっしゃいある',
-            'sキーで退店して次のステージへ',
+            'sキーで購入',
+            'Sキーで退店して次のステージへ',
         ])
 
     def next(self, key):
-        if key == 's':
+        if key == 'q':
+            self.__player.x -= 1
+            self.__player.y -= 1
+        elif key == 'a':
+            self.__player.x -= 1
+        elif key == 'z':
+            self.__player.x -= 1
+            self.__player.y += 1
+        elif key == 'w':
+            self.__player.y -= 1
+        elif key == 'x':
+            self.__player.y += 1
+        elif key == 'e':
+            self.__player.x += 1
+            self.__player.y -= 1
+        elif key == 'd':
+            self.__player.x += 1
+        elif key == 'c':
+            self.__player.x += 1
+            self.__player.y += 1
+        elif key == 's':
+            item = self.__parts_lines[self.__player.y][self.__player.x]
+            if item is None:
+                Canvas.store(['もうないよ'])
+                return self
+
+            self.__parts_lines[self.__player.y][self.__player.x] = None
+            self.__player.pick_up(item)
+            Canvas.store(['どうもね'])
+        elif key == 'S':
             field = FieldGenerator.generate_by_level(self.__next_level)
             from scenes.stage import Stage
             return Stage(field, self.__player, self.__next_level)
         else:
-            return self
+            Canvas.store(['正しく入力してください'])
+
+        if self.__player.x > 5:
+            self.__player.x = 5
+        elif self.__player.x < 0:
+            self.__player.x = 0
+        if self.__player.y > 4:
+            self.__player.y = 4
+        elif self.__player.y < 0:
+            self.__player.y = 0
+
+
+        return self
