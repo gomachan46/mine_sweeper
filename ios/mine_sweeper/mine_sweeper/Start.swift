51370b68ac1e2309b53d8261cf632a76a14334ec
gomachan46 <shiro.gomachan46@gmail.com>
2019-01-23 13:28:59 +0900

    まぁ文字はいらんやろ

--- ios/mine_sweeper/mine_sweeper/Start.swift
+++ ios/mine_sweeper/mine_sweeper/Start.swift
@@ -6,16 +6,6 @@ class Start : Tile {
    override init(position: CGPoint, rectOf size: CGSize) {
        super.init(position: position, rectOf: size)
        self.fillColor = .brown
-
-       let label = SKLabelNode(text: "S").apply { l in
-           l.fontName = "Menlo"
-           l.fontSize = CGFloat(self.rect.width)
-           l.fontColor = .black
-           l.verticalAlignmentMode = .center
-           l.horizontalAlignmentMode = .center
-           l.position = CGPoint(x: self.rect.width / 2, y: self.rect.height / 2)
-       }
-       self.addChild(label)
    }
 
    required init?(coder aDecoder: NSCoder) {
